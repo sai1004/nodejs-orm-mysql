aa422bb4dd19e612875a1bcc0efb4573133c3485
sai1004 <saikiran.bavandla@gmail.com>
2019-08-06 18:57:07 +0530

    commit

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import { createConnection, Connection } from "typeorm";
 import { Profile } from "./entities/Profile";
-import { connect } from "http2";
+import { Country } from './entities/Country';
 
 createConnection({
   type: "mysql",
@@ -23,6 +23,13 @@ createConnection({
     profile.email = "rajkumar123@gmail.com";
     profile.password = "!@#123rajk*";
 
+
+    let country = new Country();
+
+    country.id = 9999 ;
+    country.c_name = 'Something';
+    country.c_code = 98765
+
     // let savedProfile = await Connection.manager.find(Profile);
     // console.log("This Recoreds All Are From DB", savedProfile);
 
@@ -34,30 +41,43 @@ createConnection({
 
     let profileRepository = Connection.getRepository(Profile); // initializing the repo to var by calling connect.getrepo
 
+
+
     await profileRepository.save(profile);
     console.log(`Profile has been Successfully Saved`);
 
-    // let savedProfiles = await profileRepository.find();  // getting all records from db using find()
-    // console.log(`all profiile records from db`, savedProfiles);
+    let savedProfiles = await profileRepository.find();  // getting all records from db using find()
+    console.log(`all profiile records from db`, savedProfiles);
   
-    let firstProfile = await profileRepository.findOne(1); //searching by name only one recored
-    console.log(`first profile from db`, firstProfile);
+    // let firstProfile = await profileRepository.findOne(1); //searching by name only one recored
+    // console.log(`first profile from db`, firstProfile);
 
-    let secondProfile = await profileRepository.findOne({ name: "narendra"}); //searching by name only one recored
-    console.log(`second profile from db`, secondProfile);
+    // let secondProfile = await profileRepository.findOne({ name: "narendra"}); //searching by name only one recored
+    // console.log(`second profile from db`, secondProfile);
 
     //=============== updating in dataBase ================\\
 
-      let profileToUpdate = await profileRepository.findOne({ name: 'raj kumar'});
-      profileToUpdate.name = 'sai prakash';
-      await profileRepository.save(profileToUpdate);
-      console.log(profileToUpdate.name)
+      // let profileToUpdate = await profileRepository.findOne({ name: 'raj kumar'});
+      // profileToUpdate.name = 'sai prakash';
+      // await profileRepository.save(profileToUpdate);
+      // console.log(profileToUpdate.name)
 
    //============== removing from DB ======================\\
 
       // let profileRemove = await profileRepository.findOne({name: 'six user'});
       // await profileRepository.remove(profileRemove);
       // console.log(`profile has been removed Successfully`, profileRemove.name )
+
+
+    let countryRepository = Connection.getRepository(Country);
+
+    // await countryRepository.save(country);
+    // console.log(`Country has been Successfully Saved`);
+
+    let savedCountry = await countryRepository.find();
+    console.log('all the recored from country ', savedCountry)
+
+
 
   })
   .catch((error: any) => console.log(error));
